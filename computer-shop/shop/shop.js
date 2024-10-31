18d1092fbc11021bd783c6ec99b3048bcdfd7afc
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2024-10-31 08:58:10 +0100

    Delete computer-shop directory

--- computer-shop/shop/shop.js
+++
@@ -1,9 +0,0 @@
-var categoriesList = document.getElementById('categoriesList');
-var categoriesBtn = document.getElementById('categoriesBtn');
-
-categoriesBtn.addEventListener("mouseover", (show) => {});
-categoriesBtn.addEventListener("mouseout", (hide) => {});
-
-categoriesBtn.onmouseover = (show) => {categoriesList.style.display = 'inline';};
-categoriesList.onmouseover = (show) => {categoriesList.style.display = 'inline';};
-onmouseout = (hide) => {categoriesList.style.display = 'none';};
